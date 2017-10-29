69e0ad1a073721fa10afc915cfd7d92a85dec81a
Michał Kozakiewicz <michal037@users.noreply.github.com>
2017-10-29 22:28:32 +0100

    It works but not everything is finished

--- source/raspberrypi/st7735s.h
+++ source/raspberrypi/st7735s.h
@@ -84,6 +84,18 @@ struct st7735s
 };
 extern const struct st7735s st7735s;
 
+/* Inversion States */
+#define st7735s_InversionOn 1
+#define st7735s_InversionOff 0
+/* Gamma States */
+#define st7735s_Gamma1 1 /* 1.0 */
+#define st7735s_Gamma2 2 /* 2.5 */
+#define st7735s_Gamma3 4 /* 2.2 */
+#define st7735s_Gamma4 8 /* 1.8 */
+/* Orientation States */
+#define st7735s_Orientation_Portrait 0
+#define st7735s_Orientation_Landscape 1
+
 #ifdef __cplusplus
 }
 #endif
