7e75d69acd50d301c0cbeaece8303d843f3edc09
Michał Kozakiewicz <michal037@users.noreply.github.com>
2017-10-29 23:26:40 +0100

    added speed test

--- source/raspberrypi/main.c
+++ source/raspberrypi/main.c
@@ -4,11 +4,15 @@
  */
 
 #include <stdio.h>
+#include <time.h>
 #include <wiringPi.h> /* Can be changed */
 #include "st7735s.h"
 
 int main(int argc, char *argv[])
 {
+    /* For speed test */
+    clock_t start, stop;
+
     printf("BEGIN TEST!\n");
 
     /* Setup WiringPi */
@@ -18,14 +22,18 @@ int main(int argc, char *argv[])
     st7735sType disp1 = st7735s.conf.createDisplay(15000000, 0, 5, -1);
 
     /* Draw 8 colours */
-    st7735s.draw.fillScreen(255, 0, 0); /* Red */
-    st7735s.draw.fillScreen(0, 255, 0); /* Green */
-    st7735s.draw.fillScreen(0, 0, 255); /* Blue */
-    st7735s.draw.fillScreen(255, 255, 0); /* Yellow */
-    st7735s.draw.fillScreen(0, 255, 255); /* Cyan */
-    st7735s.draw.fillScreen(255, 0, 255); /* Magenta */
-    st7735s.draw.fillScreen(255, 255, 255); /* White */
-    st7735s.draw.fillScreen(0, 0, 0); /* Black */
+    start = clock();
+        st7735s.draw.fillScreen(255, 0, 0); /* Red */
+        st7735s.draw.fillScreen(0, 255, 0); /* Green */
+        st7735s.draw.fillScreen(0, 0, 255); /* Blue */
+        st7735s.draw.fillScreen(255, 255, 0); /* Yellow */
+        st7735s.draw.fillScreen(0, 255, 255); /* Cyan */
+        st7735s.draw.fillScreen(255, 0, 255); /* Magenta */
+        st7735s.draw.fillScreen(255, 255, 255); /* White */
+        st7735s.draw.fillScreen(0, 0, 0); /* Black */
+    stop = clock();
+
+    printf("One fillScreen: ~%.1f seconds!\n", (double)((stop - start)/CLOCKS_PER_SEC) / 8);
 
     /* Destroy active display */
     st7735s.conf.destroyActiveDisplay();
